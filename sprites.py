0e3b200371aadb41ace8fb54a5fac4d869739f91
amisha <129302600+amisha1816@users.noreply.github.com>
2023-05-11 10:17:18 -0400

    Update sprites.py

--- sprites.py
+++ sprites.py
@@ -10,6 +10,6 @@ class Generic(pg.sprite.Sprite):
         self.z = z
         
         # Amisha added this part in on May 11!
-       self.hitbox = self.rect.copy().inflate(-self.rect.width * 0.2, -self.rect.height * 0.75)
+       self.hitbox = self.rect.copy().inflate(-self.rect.width * 0.2, -self.rect.height * 0.75) # adjusting the size of the hitbox based off our sprites!
        # ** WILL NEED TO ADJUST NUMBERS BASED OF OUR ACTUAL SPRITES **
         
