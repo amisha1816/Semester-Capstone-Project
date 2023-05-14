f40ebba3f1986e8eda85a9422ab89dabed6e89cd
Aroush Syed <111580513+ASyed2106@users.noreply.github.com>
2023-05-13 23:36:55 -0400

    Update setting.py
    i added a few new layers

--- setting.py
+++ setting.py
@@ -1,22 +1,20 @@
 #setting
-
 # screen variables 
 w=1300
 h=800
+tile_size = 32
 
 OVERLAYER_POSITIONS = {
     'tool' : (35, h),
     'seed': (80,h)
-
 }
 
-LAYERS = {
-    'water': 0,
-    'ground': 1,
-    'soil': 2,
-    'rain floor': 3,
-    'house bottom': 4,
-    'ground plant': 5,
+LAYERS = { # layers are in level of what is at the bottom to what is on top 
+    'ground': 0,
+    'House Walls/bottom': 1,
+    'Hills': 2,
+    'Water': 3,
+    'Flowers/trees': 4,
+    'Furniture': 5,
     'main': 6
-
 }
