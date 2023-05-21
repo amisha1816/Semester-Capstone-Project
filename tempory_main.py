dc10e4fa725a4dfb5c79e4be44d7f3c905db28ae
amisha <129302600+amisha1816@users.noreply.github.com>
2023-05-21 09:18:23 -0400

    Update tempory_main.py

--- tempory_main.py
+++ tempory_main.py
@@ -4,17 +4,20 @@ https://www.youtube.com/watch?v=G8MYGDf_9ho
 
 ```python
 
+# imports
 import pygame as pg
 import sys
 from level import Level
 from button import Button
 
+# initial set-up
 pg.init() # initializing pygame
 screen = pg.display.set_mode((1300,800)) # creating screen
 pg.display.set_caption('Farming Tales')
 clock = pg.time.Clock()
 level = Level() 
         
+# button creation using button class 
 fm_button = Button(30, 30)
 home_button = Button(10, 20)
       
