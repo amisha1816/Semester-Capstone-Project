4b095b855cf5708a4a23ddab55aba717b2e1d7a4
amisha <129302600+amisha1816@users.noreply.github.com>
2023-05-20 23:04:36 -0400

    Update Button.py

--- Button.py
+++ Button.py
@@ -17,17 +17,12 @@ class Button(): # Class that allows the player can press buttons
     self.text = main_font.render(self.text_input, True, "yellow")
     self.text_rect = self.text.get_rect(center = (self.x_pos, self.y_pos))
  
-def update(self):
+def update(self): # allows us to place image along with text on screen
     screen.blit(self.image, self.rect) # allows us to put our image onto the game screen
     screen.blit(self.image, self.rect) # allows us to place the text for our button onto the game screen
 
-def mouse_check(self, position):
+def mouse_check(self, position): # checks for possible button click
     if position[0] in range(self.rect.left, self.rect.right) and position[1] in range(self.rect.top, self.rect.bottom): # checks if the mouse's position is in the button's area
         print("The button has been pressed")
         return True
     return False
-
-button_display = pg.image.load(#____add in button title____)
-# add in line adjusting the size of the button using pg.transform.scale
-    
-button = Button(button_surface, 300, 700, "Farmer's Market")
