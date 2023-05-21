88303fc5fd451f71abc073650676d06d84921c5b
amisha <129302600+amisha1816@users.noreply.github.com>
2023-05-20 23:12:25 -0400

    Update main.py

--- main.py
+++ main.py
@@ -8,14 +8,12 @@ clock = pg.time.Clock()
 pg.display.set_caption('Farming Tales')
 level = Level() 
         
-def main_page(): # method that runs the main page of our game
+def island(): # method that runs the main page of our game
     
     while True:
         
         mouse_pressing_button = pygame.mouse.get_post() # checks if we pressed any button
-        
-        island_button = Button(image = pg.imade.load("-------------------"), pos=("------"),
-                               text_input = "ISLAND", font = get_font("----"), base_color = "_______", colour = "---------")
+       
         fm_button = Button(image = pg.imade.load("-------------------"), pos=("------"),
                                text_input = "ISLAND", font = get_font("----"), base_color = "_______", colour = "---------")
         
@@ -25,8 +23,8 @@ def main_page(): # method that runs the main page of our game
                 sys.exit() # allows us to end the program
                 
             if event.type == pygame.MOUSEBUTTONDOWN:
-                if play_back.checkForInput(Play_Mouse_POS)
-                main_menu()
+                if play_back.checkForInput(Play_Mouse_POS):
+                    menu()
                 
             
         delta_time=clock.tick(120)/500
@@ -39,20 +37,53 @@ def farmers_market():
         
         mouse_pressing_button = pygame.mouse.get_post() # checks if we pressed any button
         
+        screen.fill("Black") # currently just here to check if everythings working
+        
+        island_button = Button(image = pg.imade.load("-------------------"), pos=("------"),
+                               text_input = "ISLAND", font = get_font("----"), base_color = "_______", colour = "---------")
+        
+        
         for event in pg.event.get(): # pg.event.get is a function that will return the list of events that can be processed one after another)
             if event.type == pg.QUIT: # event.type --> pg object for representing events. pg handles all its event messaging through an event queue. 
                 pg.quit() # essentially quitting game
                 sys.exit() # allows us to end the program
                 
             if event.type == pygame.MOUSEBUTTONDOWN:
-                if play_back.checkForInput(Play_Mouse_POS)
-                main_menu()
+                if play_back.checkForInput(Play_Mouse_POS):
+                    menu()
                 
             
         delta_time=clock.tick(120)/500
         pg.display.update() # allows the background to update based on our actions
  
-main_page() # he just ran the function like this? I'm not sure if we'll need to add anything ❔
+def menu():
+    
+    while True:
+        
+        # Current test
+        screen.fill("Black") # currently just here to check if everythings working
+        
+        # button
+        mouse_pressing_button = pygame.mouse.get_post() # checks if we pressed any button
+        island_button = Button(image = pg.imade.load("-------------------"), pos=("------"),
+                               text_input = "ISLAND", font = get_font("----"), base_color = "_______", colour = "---------")
+        
+        # base
+        for event in pg.event.get(): # pg.event.get is a function that will return the list of events that can be processed one after another)
+            if event.type == pg.QUIT: # event.type --> pg object for representing events. pg handles all its event messaging through an event queue. 
+                pg.quit() # essentially quitting game
+                sys.exit() # allows us to end the program
+                
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if play_back.checkForInput(Play_Mouse_POS):
+                    menu()
+                
+            
+        delta_time=clock.tick(120)/500
+        pg.display.update() # allows the background to update based on our actions
+  
+        
+menu() # he just ran the function like this? I'm not sure if we'll need to add anything ❔
 -----------------------------------------------
 
 # Possible Changes to our code (I'm not sure if this would work) 🌷 AM
