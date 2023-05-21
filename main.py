cf4e8076439543de45e9f1c016bff9de897f70d4
amisha <129302600+amisha1816@users.noreply.github.com>
2023-05-20 23:02:22 -0400

    Working on making buttons functional.py

--- main.py
+++ main.py
@@ -9,15 +9,45 @@ pg.display.set_caption('Farming Tales')
 level = Level() 
         
 def main_page(): # method that runs the main page of our game
+    
     while True:
         
-        # 🌷AM
-        mouse_pressing_button = pygame.mouse.get_post()
+        mouse_pressing_button = pygame.mouse.get_post() # checks if we pressed any button
+        
+        island_button = Button(image = pg.imade.load("-------------------"), pos=("------"),
+                               text_input = "ISLAND", font = get_font("----"), base_color = "_______", colour = "---------")
+        fm_button = Button(image = pg.imade.load("-------------------"), pos=("------"),
+                               text_input = "ISLAND", font = get_font("----"), base_color = "_______", colour = "---------")
         
         for event in pg.event.get(): # pg.event.get is a function that will return the list of events that can be processed one after another)
             if event.type == pg.QUIT: # event.type --> pg object for representing events. pg handles all its event messaging through an event queue. 
                 pg.quit() # essentially quitting game
                 sys.exit() # allows us to end the program
+                
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if play_back.checkForInput(Play_Mouse_POS)
+                main_menu()
+                
+            
+        delta_time=clock.tick(120)/500
+        pg.display.update() # allows the background to update based on our actions
+        
+        
+def farmers_market():
+    
+    while True:
+        
+        mouse_pressing_button = pygame.mouse.get_post() # checks if we pressed any button
+        
+        for event in pg.event.get(): # pg.event.get is a function that will return the list of events that can be processed one after another)
+            if event.type == pg.QUIT: # event.type --> pg object for representing events. pg handles all its event messaging through an event queue. 
+                pg.quit() # essentially quitting game
+                sys.exit() # allows us to end the program
+                
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if play_back.checkForInput(Play_Mouse_POS)
+                main_menu()
+                
             
         delta_time=clock.tick(120)/500
         pg.display.update() # allows the background to update based on our actions
