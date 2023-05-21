cc42b418dd93efb3b9213f72b0c2054717ef87d7
amisha <129302600+amisha1816@users.noreply.github.com>
2023-05-21 02:21:29 -0400

    Update and rename tempory_main.md to tempory_main.py

---
+++ tempory_main.py
@@ -0,0 +1,38 @@
+
+### Main
+https://www.youtube.com/watch?v=G8MYGDf_9ho
+
+```python
+
+import pygame as pg
+import sys
+from level import Level
+from button import Button
+
+pg.init() # initializing pygame
+screen = pg.display.set_mode((1300,800)) # creating screen
+pg.display.set_caption('Farming Tales')
+clock = pg.time.Clock()
+level = Level() 
+        
+fm_button = Button(30, 30)
+home_button = Button(10, 20)
+      
+# Game loop  
+while run:
+        
+        if fm_button.draw(surface):
+            print("IT WORKS")
+        if home_button.draw(surface)
+            print("Home is working")
+        
+        for event in pg.event.get(): 
+            if event.type == pg.QUIT: 
+                pg.quit() # essentially quitting game
+                sys.exit() # allows us to end the program
+
+        delta_time=clock.tick(120)/500
+        pg.display.update() 
+        
+ 
+
