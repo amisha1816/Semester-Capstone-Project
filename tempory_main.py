6137e0e06aebe2a3d28941ae2cfea8b914a19146
amisha <129302600+amisha1816@users.noreply.github.com>
2023-05-21 10:10:27 -0400

    Update tempory_main.py

--- tempory_main.py
+++ tempory_main.py
@@ -10,20 +10,23 @@ pg.init() # initializing pygame
 screen = pg.display.set_mode((1300,800)) # creating screen
 pg.display.set_caption('Farming Tales')
 clock = pg.time.Clock()
-level = Level() 
-        
-# button creation using button class 
-fm_button = Button(30, 30)
-home_button = Button(10, 20)
-      
+level = Level()
+town_button = pressed
+new_press = True # checks if we just pressed the button
+    
 # Game loop  
 while run:
-        
-        if fm_button.draw(surface):
-            print("IT WORKS")
-        if home_button.draw(surface)
-            print("Home is working")
-        
+    # button creation using button class 
+    town_button = Button("Town Button", 10, 20, town_button_work)
+    fm_button = Button("Farmer's Market", 30, 30, fm_button_work)
+
+    # Checks if buttons have been clicked
+    if pg.mouse.get_pressed()[0] and new_press:
+        new_press = False
+
+    if not pg.mouse.get_pressed()[] and not new_press:
+        new_press = True
+ 
         for event in pg.event.get(): 
             if event.type == pg.QUIT: 
                 pg.quit() # essentially quitting game
