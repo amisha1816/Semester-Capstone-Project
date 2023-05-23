4c91aae587160eba3d8803aa26302725c2bfcff2
Aroush Syed <111580513+ASyed2106@users.noreply.github.com>
2023-05-23 10:49:31 -0400

    Update tempory_main.py
    -debugged

--- tempory_main.py
+++ tempory_main.py
@@ -1,5 +1,4 @@
 
-# imports
 import pygame as pg
 import sys
 from level import Level
@@ -11,29 +10,27 @@ screen = pg.display.set_mode((1300,800)) # creating screen
 pg.display.set_caption('Farming Tales')
 clock = pg.time.Clock()
 level = Level()
-town_button = pressed
+#town_button = pressed
 new_press = True # checks if we just pressed the button
     
 # Game loop  
+run = True 
 while run:
     # button creation using button class 
-    town_button = Button("Town Button", 10, 20, town_button_work)
-    fm_button = Button("Farmer's Market", 30, 30, fm_button_work)
+    town_button = Button( 10, 20,"Town Button")
+    fm_button = Button(30, 30,"Farmer's Market")
 
     # Checks if buttons have been clicked
-    if pg.mouse.get_pressed()[0] and new_press:
-        new_press = False
+    #if pg.mouse.get_pressed()[0] and new_press:
+     #   new_press = False
 
-    if not pg.mouse.get_pressed()[] and not new_press:
-        new_press = True
+    # not pg.mouse.get_pressed()[] and not new_press:
+        #new_press = True
  
-        for event in pg.event.get(): 
-            if event.type == pg.QUIT: 
-                pg.quit() # essentially quitting game
-                sys.exit() # allows us to end the program
-
+    for event in pg.event.get(): # pg.event.get is a function that will return the list of events that can be processed one after another)
+        if event.type == pg.QUIT: # event.type --> pg object for representing events. pg handles all its event messaging through an event queue. 
+            pg.quit() # essentially quitting game
+            sys.exit() # allows us to end the program
         delta_time=clock.tick(120)/500
-        pg.display.update() 
-        
- 
-
+        level.run(delta_time)
+        pg.display.update()
