29cbdd16db4dd5387fcf5a823f252b413187c9b0
amisha <129302600+amisha1816@users.noreply.github.com>
2023-05-30 11:22:40 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,27 @@
 
+# This is the new version from April 30th
+
+import pygame as pg
+import sys
+from level import Level
+from button import Button
+
+# initial set-up
+pg.init()
+pg.display.set_caption('Farming Tales')
+clock = pg.time.Clock()
+screen = pg.display.set_mode((1300,800))
+level = Level()
+
+# Game loop  
+run = True 
+while run:
+ 
+    for event in pg.event.get():
+        if event.type == pg.QUIT:
+            pg.quit() 
+            sys.exit()
+
+    pg.display.update() # allows a portion of the screen to be blitted
+    delta_time = clock.tick(120)/500
+    level.run(delta_time) 
