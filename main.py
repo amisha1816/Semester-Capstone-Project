c0b08c3295a207b00a15ceb5ce48e07c91c6b005
Aroush Syed <111580513+ASyed2106@users.noreply.github.com>
2023-06-03 23:05:54 -0400

    Update main.py

--- main.py
+++ main.py
@@ -25,3 +25,19 @@ while run:
     pg.display.update() # allows a portion of the screen to be blitted
     delta_time = clock.tick(120)/500
     level.run(delta_time) 
+
+'''
+KEYS BEING USED:
+
+up: K_UP
+down: K_DOWN
+left:K_LEFT
+right:K_RIGHT
+using axe: K_a
+using hoe:K_s
+switching tools:K_q
+switching seeds:K_e
+new day: K_TAB
+entering farmers market: K_RETURN
+leaving farmers market: K_ESCAPE
+seed timer: K_LCTRL'''
