7f9255e4ff74f7140e5d0789481e391669281d0a
Aroush Syed <111580513+ASyed2106@users.noreply.github.com>
2023-06-03 21:54:20 -0400

    Update gardening.py

--- gardening.py
+++ gardening.py
@@ -8,3 +8,15 @@ class Dirt_Bloop(pg.sprite.Sprite):
         self.image = surf
         self.rect=self.image.get_rect(topleft=pos)
         self.z=LAYERS['dirt_dirt']
+
+class Ground_Dirt:
+    def __init__(self,all_sprites):
+        self.all_sprites = all_sprites
+        self.dirt=pg.sprite.Group()
+        self.dirt_image=pg.image.load('Sprout Lands - Sprites - Basic pack/Objects/dirt.png')
+        self.dirt_grid()
+        self.hit_rect()
+        #requirements
+        # if the area is farmable
+        # if the soil has been watered
+        # is the soil has a plant on it
