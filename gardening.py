a7f58cff4e9866c9df78e4a1ae7db064835cfabc
Aroush Syed <111580513+ASyed2106@users.noreply.github.com>
2023-06-03 21:54:00 -0400

    Update gardening.py

--- gardening.py
+++ gardening.py
@@ -1,3 +1,10 @@
 import pygame as pg
 from setting import *
 from pytmx.util_pygame import load_pygame
+
+class Dirt_Bloop(pg.sprite.Sprite):
+    def __init__(self,pos,surf,groups):
+        super().__init__(groups)
+        self.image = surf
+        self.rect=self.image.get_rect(topleft=pos)
+        self.z=LAYERS['dirt_dirt']
