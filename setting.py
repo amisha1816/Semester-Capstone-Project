d14dfa622e1941609aa6af83c1a0a97dc31220f5
amisha <129302600+amisha1816@users.noreply.github.com>
2023-06-05 19:21:41 -0400

    Update setting.py
    Setup the random price generation!

--- setting.py
+++ setting.py
@@ -1,3 +1,5 @@
+import random # I thought this could be fun to add!
+
 #setting
 # screen variables 
 w=1300
@@ -22,3 +24,17 @@ LAYERS = { # layers are in level of what is at the bottom to what is on top
 APPLE_POSITION= {
     'tree': [(-5,78),(18,17),(50,27),(30,37),(80,47),(90,10)]
 }
+
+sell_prices = [1, 1, 4, 6, 8, 11, 12, 14, 16, 18, 21, 1, 24, 1, 28, 1, 32, 1, 36, 1, 1, 42, 44, 1, 1, 51, 1, 54, 1, 1, 60, 1, 1, 66, 1, 71, 1, 1, 1, 78, 81, 1, 84, 86, 1, 91, 1, 1, 96, 81, 10000]
+sell_random_select = sell_prices[random.randint(1, 49)]
+
+if self.fm_active:
+    SELL_STUFF = {
+            'apple': sell_random_select,
+            'corn': sell_random_select,
+            'tomato': sell_random_select
+            }
+
+    BUY_STUFF = {
+            'corn_seed': 5, 
+            'tomato_seed': 5 
