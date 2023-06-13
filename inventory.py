212ebbab0690e38bc47a8d01571da1609d23dbc1
Aroush Syed <111580513+ASyed2106@users.noreply.github.com>
2023-06-13 17:41:50 -0400

    Update inventory.py

--- inventory.py
+++ inventory.py
@@ -9,6 +9,158 @@ class Inventory:
   
         self.character = character
         self.inventory_menu = inventory_menu
+        self.font = pg.font.SysFont('Cambria', 24)
+        self.images = ['Sprout Lands - Sprites - Basic pack/Objects/wood.png','Sprout Lands - Sprites - Basic pack/Objects/applee.png','Sprout Lands - Sprites - Basic pack/Objects/corn.png','Sprout Lands - Sprites - Basic pack/Objects/tomatos.png','Sprout Lands - Sprites - Basic pack/Objects/seed  bag.png','Sprout Lands - Sprites - Basic pack/Objects/seed  bag.png','Sprout Lands - Sprites - Basic pack/Characters/cow.webp' ] # ❗ insert item images here
+        
+         # options list
+        self.options = list(self.character.crop_stuff.keys()) + list(self.character.seed_stuff.keys())
+        
+        # rows
+        self.first_row = self.options[:3]
+        self.second_row = self.options[3:]
+
+        # selection
+        self.index = 0
+        self.timer = Timer(200)
+        
+        # spacing and padding
+        self.v_space = 30 # space between the different rows
+        self.h_space = 30 # space btwn the different blocks within the row
+        
+        # sizing 
+        self.total_width = 1000
+        self.total_height = 600
+        self.block_width = (self.total_width - (self.h_space * 4)) / 3
+        self.block_height = (self.total_height - (self.v_space * 3)) / 2
+        # so based on my current math, each block should be 180 by 160        
+
+        self.txt_bg()
+                             
+# ___________________________________________________________________________________________________________________________
+
+    def txt_bg(self): # creates our text and background
+        
+        # text
+        self.item_names = [] 
+        
+        # rendering text and additing it to our text_item
+        for item in self.options:
+            self.item_name = self.font.render(item, False, 'Black')    
+            self.item_names.append(item)            
+            
+ 
+        self.top = h / 2 - self.total_height / 2
+        self.background = pg.Rect(150, self.top, self.total_width, self.total_height) # background that pulls everything together ❗ 400
+
+# ___________________________________________________________________________________________________________________________
+    
+    def select_stuff(self):
+        keys = pg.key.get_pressed() 
+        self.timer.update_tool()
+
+        if keys[pg.K_SLASH]: # if player hits escape, display closes
+            self.inventory_menu()
+
+        if not self.timer.start():
+
+            if keys[pg.K_g]:
+                self.index -= 1
+                self.timer.start()
+
+            if keys[pg.K_h]:
+                self.index += 1
+                self.timer.start()
+
+            if keys[pg.K_DOWN]:
+                self.index += len(self.first_row)
+                self.timer.start()
+
+            if keys[pg.K_UP]:
+                self.index -= len(self.first_row)
+                self.timer.start()
+                    
+        # restrict selection
+        if self.index < 0:
+            self.index = len(self.options) - 1 
+        
+        if self.index > len(self.options) - 1:
+            self.index = 0
+                            
+# ___________________________________________________________________________________________________________________________ 
+
+    def block(self, item_name, left, top, amount, img, chosen): # creates blocks (bg, text, amount, border)
+
+        block_bg = pg.Rect(left, top, self.block_width, self.block_height)
+
+        # pos (l) → defined later within update method
+        # top (t) → distance from top that's defined in update method
+        # self.block_width → created in init
+        # self.block_height → created in txt_bg method
+
+        pg.draw.rect(screen, 'White', block_bg, 0, 5)
+        # 🌱 ^ this line blits the blocks to the screen
+
+        # text
+        text = self.font.render(str(item_name), False, 'Black')
+        text_rect = text.get_rect(bottomleft = (block_bg.left + 40, block_bg.bottom - 60))
+        screen.blit(text, text_rect)
+         # 🌱 ^ this line blits the text to the screen
+    
+        # amounts
+        amount_text = self.font.render(str(amount), False, 'Black')
+        amount_block = amount_text.get_rect(bottomleft = (block_bg.left + 40, block_bg.bottom - 20))
+        screen.blit(amount_text, amount_block)
+
+        # images
+        img_position = img.get_rect(center = (block_bg.centerx-10, block_bg.centery-40)) # 🐋
+        screen.blit(img, img_position) # 🐋
+        
+        if chosen:
+            pg.draw.rect(screen, 'black', block_bg, 6, 5) # border when item is selected (this doesn't really do much
+            
+# ___________________________________________________________________________________________________________________________ 
+            
+    def update(self):
+        self.select_stuff()
+        pg.draw.rect(screen, 'Brown', self.background)
+
+        for item_index, item_name in enumerate(self.item_names):
+            
+            if item_index <= 2:
+                top = self.top + self.v_space
+                left = self.background.left + (self.block_width * item_index) + (self.h_space * (item_index + 1))
+                # img_position = (self.background.left + (self.block_width * self.index) + (self.h_space * (self.index + 1) + 20), top)
+                # + 20 is just for an inden
+   
+            if item_index >= 3:
+                top = self.top + self.v_space + self.block_height + self.v_space
+                left = self.background.left + (self.block_width * (item_index-3)) + (self.h_space * ((item_index-3) + 1))
+                # img_position = (self.background.left + (self.block_width * item_name.index) + (self.h_space * (self.index + 1) + 20), top)
+            
+
+            # img
+            img_link = self.images[item_index] 
+            img = pg.image.load(img_link) 
+            img=pg.transform.scale(img,(120,120))
+
+            # amount
+            amount_list = list(self.character.crop_stuff.values()) + list(self.character.seed_stuff.values())
+            amount = amount_list[item_index]
+            
+            self.block(item_name, left, top, amount, img, self.index == item_index) 
+ 
+
+'''import pygame as pg
+from setting import *
+from timer import Timer
+screen = pg.display.set_mode((w,h))
+
+# main class 
+class Inventory:
+    def __init__(self, character, inventory_menu): # basic set_up
+  
+        self.character = character
+        self.inventory_menu = inventory_menu
         self.font = pg.font.SysFont('Cambria', 30)
         self.images = ['Sprout Lands - Sprites - Basic pack/Characters/cow.webp','Sprout Lands - Sprites - Basic pack/Characters/cow.webp','Sprout Lands - Sprites - Basic pack/Characters/cow.webp','Sprout Lands - Sprites - Basic pack/Characters/cow.webp','Sprout Lands - Sprites - Basic pack/Characters/cow.webp','Sprout Lands - Sprites - Basic pack/Characters/cow.webp' ] # ❗ insert item images here
         
@@ -119,7 +271,7 @@ class Inventory:
         screen.blit(img, img_position) # 🐋
         
         if chosen:
-            pg.draw.rect(screen, 'green', block_bg, 6, 5) # border when item is selected (this doesn't really do much
+            pg.draw.rect(screen, 'green', block_bg, 6, 5) # border when item is selected (this doesn't really do much'''
             
 # ___________________________________________________________________________________________________________________________ 
             
